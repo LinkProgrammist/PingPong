b9c7f2fde13684d1d9c2210be50c8dc4102ed238
Линк_программист <110724995+LinkProgrammist@users.noreply.github.com>
2022-08-07 11:24:22 +0300

    Update pingpong.py
    Добавлены классы GameSprite и Player. Созданы и отрисованы экземпляры этих классов.

--- pingpong.py
+++ pingpong.py
@@ -1,5 +1,32 @@
-from turtle import window_height, window_width
 from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        sprite.Sprite.__init__(self)
+
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player_speed
+
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_RIGHT] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
 back = (200, 255, 255)
 win_width = 600
@@ -12,9 +39,18 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
+
     display.update()
     clock.tick(FPS)
